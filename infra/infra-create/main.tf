1c8b53eeaddcfea9423f18dc5b88615e35323d22
akga0822 <akshay.gaddam@netcracker.com>
2024-10-24 23:18:34 +0530

    infra-tool-1

--- infra/infra-create/main.tf
+++ infra/infra-create/main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "tool-sg"{
   ingress  {
     from_port = 22
     to_port = 22
-    protocol = "-1"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags={
