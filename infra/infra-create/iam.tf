53c81eaa453c96aa3524dffd8fb29b00163d9665
akga0822 <akshay.gaddam@netcracker.com>
2024-10-26 16:10:22 +0530

    role change

---
+++ infra/infra-create/iam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "admin_role"{
+  name = "${var.name}-role"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.name}-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "policy-attach"{
+  count = length(var.policy_name)
+  role = aws_iam_role.admin_role.name
+  policy_arn = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,7 @@ module "infra-create"{
   source = "./infra-create"
   instance_type = each.value["instance_type"]
   name = each.key
+  policy_name = each.value["policy_name"]
 }
 
 
