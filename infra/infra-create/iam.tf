3228b7202e43f84f40bdce89102267dd5513b6f3
akga0822 <akshay.gaddam@netcracker.com>
2024-10-26 16:36:42 +0530

    role change

--- infra/infra-create/iam.tf
+++ infra/infra-create/iam.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "admin_role"{
 
 resource "aws_iam_instance_profile" "github-iam-instance-profile" {
   name = "${var.name}-profile"
-  role = aws_iam_role.admin_role
+  role = aws_iam_role.admin_role.name
 }
 resource "aws_iam_role_policy_attachment" "policy-attach"{
   count = length(var.policy_name)
