92b0110c66c47fce9902c7c18aa30ff4a2b25354
akga0822 <akshay.gaddam@netcracker.com>
2024-10-30 23:12:58 +0530

    tools-infra

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "name" {
   egress {
     from_port = 0
     to_port = 0
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
     protocol = "-1"
   }
   ingress {
