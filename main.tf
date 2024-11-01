b95f32b052d70f410f406fbcb2e395857aa8d929
akga0822 <akshay.gaddam@netcracker.com>
2024-11-01 12:05:18 +0530

    tools-infra

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module outputs {
   name = "test"
   bastion_nodes = var.bastion_nodes
   instance_type = "test"
-  allow_sg_cidr = "test"
+  allow_sg_cidr = "0.0.0.0/0"
   asg = false
   allow_port = 111
   subnet_ids = ["1.1.1.1"]
