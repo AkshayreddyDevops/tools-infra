9e3f20be61a459317cdb5061b9d3df20f0b47783
akga0822 <akshay.gaddam@netcracker.com>
2024-11-06 15:16:46 +0530

    test

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "app_ec2" {
   allow_lb_sg_cidr = each.value["allow_lb_sg_cidr"]
   acm_https_arn = each.value["https_acs_arn"]
   # dns_name = module.vpc.subnet[each.value["subnet_ref"]]
-  dns_name = module.loadbalance.dns_name[each.value["lb_ref"]]
+  dns_name = module.loadbalance[each.value["lb_ref"]].dns_name
 }
 module "db" {
   depends_on = [ module.vpc ]
